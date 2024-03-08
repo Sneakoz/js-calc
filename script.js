861a6b5f5e699790bf52339c97592b3da785e5bc
Sneakoz <79712322+Sneakoz@users.noreply.github.com>
2024-03-08 17:01:59 +0500

    import rest of the buttons

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ let firstNum;
 let secondNum;
 let operator;
 
-// Create event listeners for the buttons
+// Import the buttons
 const numButtons = document.querySelectorAll('.number');
+const sumBtn = document.querySelector('#sum');
+const subtractBtn = document.querySelector('#subtract');
+const multiplyBtn = document.querySelector('#multiply');
+const divideBtn = document.querySelector('#divide');
+const equalBtn = document.querySelector('#equal');
+const clearBtn = document.querySelector('#clear');
 
+// Create event listeners for the buttons
 numButtons.forEach(button => {
     button.addEventListener('click', () => {
         let text = button.textContent;
         console.log(text);
     })
-})
+});
 
 
 // Create the operate function
 function operate(first, second, op) {
     return add(first, second);
-}
+};
 
 // Create the operator functions
 function add(a, b) {
