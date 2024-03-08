02bb8afb7b05fab6483b936cf602942335f8b01c
Sneakoz <79712322+Sneakoz@users.noreply.github.com>
2024-03-08 23:59:25 +0500

    lod of shit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Create the number and operator variables
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let operatorPressed = false;
 
 // Import the buttons
 const numButtons = document.querySelectorAll('.number');
+const opButtons = document.querySelectorAll('.operator');
 const sumBtn = document.querySelector('#sum');
 const subtractBtn = document.querySelector('#subtract');
 const multiplyBtn = document.querySelector('#multiply');
@@ -12,14 +14,34 @@ const divideBtn = document.querySelector('#divide');
 const equalBtn = document.querySelector('#equal');
 const clearBtn = document.querySelector('#clear');
 
-// Create event listeners for the buttons
+// Create event listeners for the numButtons
 numButtons.forEach(button => {
     button.addEventListener('click', () => {
         let text = button.textContent;
         console.log(text);
-    })
+        if (operatorPressed == false) {
+            firstNum += text;
+            console.log(firstNum);
+        } else {
+            secondNum += text;
+            console.log(secondNum);
+        }
+    });
 });
 
+// Create event listeners for operator buttons
+opButtons.forEach(button => {
+    button.addEventListener('click', ()=> {
+        operator = opButtons.textContent;
+        operatorPressed = true;
+        console.log(operator);
+    });
+});
+
+// subtractBtn.addEventListener('click', () => );
+// subtractBtn.addEventListener('click', () => );
+// subtractBtn.addEventListener('click', () => );
+// subtractBtn.addEventListener('click', () => );
 
 // Create the operate function
 function operate(first, second, op) {
