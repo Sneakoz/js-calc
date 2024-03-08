f0d0878f83d39e3057bd821f2ebc018f1ee5a18c
Sneakoz <79712322+Sneakoz@users.noreply.github.com>
2024-03-08 16:34:48 +0500

    loads os shi

---
+++ script.js
@@ -0,0 +1,28 @@
+// Create the number and operator variables
+let firstNum;
+let secondNum;
+let operator;
+
+// 
+
+// Create the operate function
+function operate(first, second, op) {
+    return add(first, second);
+}
+
+// Create the operator functions
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a , b) {
+    return a / b;
+};
